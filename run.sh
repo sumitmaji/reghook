0923d706a520763ae90514c17403398c88e4047e
root <root@master.cloud.com>
2018-06-15 10:34:19 +0530

    Updated port mapping

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source scripts/config
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
-docker run -v ~/.kube:/root/.kube -v $BUILD_PATH:/tmp -p 5002:5002 -d --name $CONTAINER_NAME $REPO_NAME
+docker run -v ~/.kube:/root/.kube -v $BUILD_PATH:/tmp -p 5003:5003 -d --name $CONTAINER_NAME $REPO_NAME
